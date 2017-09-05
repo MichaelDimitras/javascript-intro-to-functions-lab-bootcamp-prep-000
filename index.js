762b4f2a987704ceeaeb57dff99098c42700124d
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-05 22:49:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,4 @@ function sayHiToGrandma(s){
   if(s === whisper(s)) return "I can't hear you!";
   else if (s === shout(s)) return "YES INDEED!";
   else if (s === "I love you, Grandma.") return "I love you, too."
-
-  }
 }
